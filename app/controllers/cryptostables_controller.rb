455d11ca3c1da79344a5f40cf5155769750de004
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-09-10 15:24:43 +0000

    more models

--- app/controllers/cryptostables_controller.rb
+++ app/controllers/cryptostables_controller.rb
@@ -1,4 +1,4 @@
 class CryptostablesController < ApplicationController
   before_action :set_cryptostable, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_a
+  before_action :correct_user, only: [:edit, :update, :d
