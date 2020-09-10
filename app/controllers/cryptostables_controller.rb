e834b766c1856910e892bfb854d9ee70bd4bdc41
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-09-10 16:17:28 +0000

    add codecov badge

--- app/controllers/cryptostables_controller.rb
+++ app/controllers/cryptostables_controller.rb
@@ -1,4 +1,7 @@
 class CryptostablesController < ApplicationController
   before_action :set_cryptostable, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :correct_user, only: [:edit, :update, :d
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
+
+  # GET /cryptostables
+  # GET /cr
