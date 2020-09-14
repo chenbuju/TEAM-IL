0ed927b4a73dadd9a45fd6619354e02656a7d2d0
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-09-14 19:36:34 +0000

    jffs2: Fix lock acquisition order bug in jffs2_write_begin

--- app/controllers/cryptostables_controller.rb
+++ app/controllers/cryptostables_controller.rb
@@ -13,4 +13,5 @@ class CryptostablesController < ApplicationController
     require 'openssl'
 
     @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV.fetch('CRYPTO')}&start=1&limit=500"
-    @uri = 
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@u
