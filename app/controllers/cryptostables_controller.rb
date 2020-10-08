8f1cffce06428a3745577c588d736a600f6b9897
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-10-08 19:14:17 +0000

    bacon: Update defconfig

--- app/controllers/cryptostables_controller.rb
+++ app/controllers/cryptostables_controller.rb
@@ -74,4 +74,6 @@ class CryptostablesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cryptostable
+      @cryptos
