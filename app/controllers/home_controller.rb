c47fce69c35cfd8ae26a3e7210b37317839d0893
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-10-14 20:36:46 +0000

    pwet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,5 @@ class HomeController < ApplicationController
 
     @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV.fetch('CRYPTO')}&start=1&limit=500"
     @uri = URI(@url)
-    @respons
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.
