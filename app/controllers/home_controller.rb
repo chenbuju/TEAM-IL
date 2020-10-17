36ed7358e7d418d3e4d3e51b107d1d70a623aa8d
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-10-17 07:11:20 +0000

    Added a new post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,4 +20,5 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'openssl'
 
-    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV.fetch('CRYPTO')}&start=1&limit=500"
+  
