23b530248a9cc19d068c9562c155b27dcfedb88e
chenbuju <107567695+chenbuju@users.noreply.github.com>
2020-10-17 14:49:00 +0000

    removed unused function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,4 +22,5 @@ class HomeController < ApplicationController
 
     @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV.fetch('CRYPTO')}&start=1&limit=500"
     @uri = URI(@url)
-    @response = Net::HTTP.g
+    @response = Net::HTTP.get(@uri)
+    @lookup_coins = JSON.parse(@resp
