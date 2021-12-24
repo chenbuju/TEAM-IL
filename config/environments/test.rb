df04981e3e5538b47849d60758b42e7fe56514eb
chenbuju <107567695+chenbuju@users.noreply.github.com>
2021-12-24 16:30:59 +0000

    fix review

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print de
+  # Print deprecation notices to the stderr.
+  config.
