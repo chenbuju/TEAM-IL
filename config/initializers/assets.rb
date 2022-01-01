86a461d309a58b8c6dac0be1241441435f105251
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-01-01 13:11:44 +0000

    fix authentication

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,5 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-# Rails.application.conf
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Ya
