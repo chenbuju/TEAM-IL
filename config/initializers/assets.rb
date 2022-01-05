dc8a48fe50eda22278ecb7521f0ff26193b3d01e
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-01-05 19:59:03 +0000

    Fixed failing unit test.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Pre
+# Precompile additional assets.
+# application.js, application.c
