42dddeb681359965c02b941ad9da604353c849ec
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-01-24 06:20:53 +0000

    Fixed README.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,4 +13,7 @@
 #   policy.style_src   :self, :https
 
 #   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-re
+#   # policy.report_uri "/csp-violation-report-endpoint"
+# end
+
+# If you are
