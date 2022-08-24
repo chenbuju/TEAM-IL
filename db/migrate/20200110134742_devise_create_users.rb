48aa21c635501705de1d5266ab45598f77e8f4c7
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-08-24 08:11:11 +0000

    added travis ci badge

--- db/migrate/20200110134742_devise_create_users.rb
+++ db/migrate/20200110134742_devise_create_users.rb
@@ -7,4 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      ## Recoverabl
+      ## Recoverable
+      t.string   :reset_password_token
+      t.
