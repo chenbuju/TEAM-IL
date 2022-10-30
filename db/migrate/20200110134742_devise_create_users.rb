004b2b00749a9544d0a0604a4c8ad5f521e8ec7a
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-10-30 18:08:23 +0000

    Release 1.2.2

--- db/migrate/20200110134742_devise_create_users.rb
+++ db/migrate/20200110134742_devise_create_users.rb
@@ -17,4 +17,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in
+      # t.datetime :last_sign_in_at
+      # t.inet     :current_sign_in_ip
