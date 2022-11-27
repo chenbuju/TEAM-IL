564a37638dccfbc386770c5f9d3bb9a8c8f11561
chenbuju <107567695+chenbuju@users.noreply.github.com>
2022-11-27 19:28:52 +0000

    staging: zcache: support multiple clients, prep for KVM and RAMster

--- db/migrate/20200110134742_devise_create_users.rb
+++ db/migrate/20200110134742_devise_create_users.rb
@@ -36,4 +36,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email,         
+    add_index :users, :email,                unique: true
+    add_index :users, :reset
