83fefb7f2f4879740dd0c133c96279ff78119cf4
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-01-15 18:30:09 +0000

    incorporate feedback

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_01_13_083124) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 
+  enable_extension "plpgsql"
+
+  create_table "cryptostables", force
