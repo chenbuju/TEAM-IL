d9dd6c3a1ad4d01784080344632f6bff0f4ced58
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-02-05 16:07:00 +0000

    eslint --fix .

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_083124) do
     t.index ["user_id"], name: "index_cryptostables_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null:
