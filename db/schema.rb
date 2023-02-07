8eeabd06bf672921ba9619a9cb269fd14011a194
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-02-07 07:09:27 +0000

    Update routes

--- db/schema.rb
+++ db/schema.rb
@@ -26,4 +26,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_083124) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null:
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", 
