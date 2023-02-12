afd8569c6cf1e737de85f14d2db6f2aaebead16f
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-02-12 09:01:02 +0000

    Add licence

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_083124) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_passw
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember
