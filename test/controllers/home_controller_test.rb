5db299989249241c923063971af9a3e4a4c72a5b
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-03-09 21:12:26 +0000

    More style updates

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,4 +3,7 @@ require 'test_helper'
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get home_index_url
-    as
+    assert_response :success
+  end
+
+end
