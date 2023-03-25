64c88b21f4744debacb5ab1f2ff2d3ba6ea1b0a1
chenbuju <107567695+chenbuju@users.noreply.github.com>
2023-03-25 05:42:07 +0000

    add more notes

--- test/system/cryptostables_test.rb
+++ test/system/cryptostables_test.rb
@@ -12,3 +12,39 @@ class CryptostablesTest < ApplicationSystemTestCase
   end
 
   test "creating a Cryptostable" do
+    visit cryptostables_url
+    click_on "New Cryptostable"
+
+    fill_in "Amount owned", with: @cryptostable.amount_owned
+    fill_in "Cost per", with: @cryptostable.cost_per
+    fill_in "Symbol", with: @cryptostable.symbol
+    fill_in "User", with: @cryptostable.user_id
+    click_on "Create Cryptostable"
+
+    assert_text "Cryptostable was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Cryptostable" do
+    visit cryptostables_url
+    click_on "Edit", match: :first
+
+    fill_in "Amount owned", with: @cryptostable.amount_owned
+    fill_in "Cost per", with: @cryptostable.cost_per
+    fill_in "Symbol", with: @cryptostable.symbol
+    fill_in "User", with: @cryptostable.user_id
+    click_on "Update Cryptostable"
+
+    assert_text "Cryptostable was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Cryptostable" do
+    visit cryptostables_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Cryptostable was successfully destroyed"
+  end
+end
